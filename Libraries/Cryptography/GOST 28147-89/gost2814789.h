d1de6798017d56e0a00dedbd5bd9b7d27d45b59c
Denis Chicherov <dippinest.workmail@gmail.com>
2025-09-29 19:36:38 +0300

    Update gost2814789.h

--- Libraries/Cryptography/GOST 28147-89/gost2814789.h
+++ Libraries/Cryptography/GOST 28147-89/gost2814789.h
@@ -39,7 +39,9 @@
 #include <stdint.h>
 
 
+#ifndef ROTL
 #define ROTL(val, pos, val_type) (val << pos) | (val >> (sizeof(val_type) * 8 - pos))
+#endif
 
 
 uint32_t GOST2814789_Encrypt_ECB(void *data, const uint32_t data_size, const void *key_256bit);
@@ -64,6 +66,7 @@ uint32_t GOST2814789_Decrypt_OFB(const void *init_vector_64bit, void *data, cons
 
 
 #endif
+
 
 
 
