3bb1dec82e682d70987fa913fc7b88eb3180d9cf
Denis Chicherov <dippinest.workmail@gmail.com>
2025-09-29 20:46:01 +0300

    Update gost2814789.h

--- Libraries/Cryptography/GOST 28147-89/gost2814789.h
+++ Libraries/Cryptography/GOST 28147-89/gost2814789.h
@@ -19,7 +19,7 @@
 //
 // Library with the implementation of the cryptographic block algorithm
 // of the GOST 28147-89 standard, developed in the USSR by the 8th directorate
-// of the KGB (Committee for State Security USSR) (Wikipedia link: https://en.wikipedia.org/wiki/GOST_(block_cipher) ).
+// of the KGB - Committee for State Security USSR (Wikipedia link: https://en.wikipedia.org/wiki/GOST_(block_cipher) ).
 //
 // This algorithm uses a Feistel network and is designed to work with
 // a 64-bit block and a 256-bit encryption key.
