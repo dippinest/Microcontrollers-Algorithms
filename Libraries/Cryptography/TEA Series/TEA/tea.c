35d46d48d63767019f163d4412d85843ae1d4ef0
Denis Chicherov <dippinest.workmail@gmail.com>
2025-09-29 18:01:27 +0300

    Update tea.c

--- Libraries/Cryptography/TEA Series/TEA/tea.c
+++ Libraries/Cryptography/TEA Series/TEA/tea.c
@@ -1,7 +1,7 @@
 
 #include "tea.h"
 
-static void _TEA_64bit_Block_Encrypt(void *_64bit_block, const void *key_128bit)
+static void _TEA_64bit_Block_Encrypt(void* _64bit_block, const void* key_128bit)
 {
 	uint32_t v0 = ((uint32_t*)_64bit_block)[0];
 	uint32_t v1 = ((uint32_t*)_64bit_block)[1];
@@ -24,7 +24,7 @@ static void _TEA_64bit_Block_Encrypt(void *_64bit_block, const void *key_128bit)
 	((uint32_t*)_64bit_block)[1] = v1;
 }
 
-static void _TEA_64bit_Block_Decrypt(void *_64bit_block, const void *key_128bit)
+static void _TEA_64bit_Block_Decrypt(void* _64bit_block, const void* key_128bit)
 {
 	uint32_t v0 = ((uint32_t*)_64bit_block)[0];
 	uint32_t v1 = ((uint32_t*)_64bit_block)[1];
@@ -47,31 +47,31 @@ static void _TEA_64bit_Block_Decrypt(void *_64bit_block, const void *key_128bit)
 	((uint32_t*)_64bit_block)[1] = v1;
 }
 
-uint32_t TEA_Encrypt_ECB(void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Encrypt_ECB(void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint32_t num_of_encrypted_bytes = 0;
 
 	for (; num_of_encrypted_bytes < data_size; num_of_encrypted_bytes += 8)
 	{
-		_TEA_64bit_Block_Encrypt(data + num_of_encrypted_bytes, key_128bit);
+		_TEA_64bit_Block_Encrypt((uint8_t*)data + num_of_encrypted_bytes, key_128bit);
 	}
 
 	return num_of_encrypted_bytes;
 }
 
-uint32_t TEA_Decrypt_ECB(void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Decrypt_ECB(void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint32_t num_of_decrypted_bytes = 0;
 
 	for (; num_of_decrypted_bytes < data_size; num_of_decrypted_bytes += 8)
 	{
-		_TEA_64bit_Block_Decrypt(data + num_of_decrypted_bytes, key_128bit);
+		_TEA_64bit_Block_Decrypt((uint8_t*)data + num_of_decrypted_bytes, key_128bit);
 	}
 
 	return num_of_decrypted_bytes;
 }
 
-uint32_t TEA_Encrypt_CBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Encrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector[8];
 
@@ -89,7 +89,7 @@ uint32_t TEA_Encrypt_CBC(const void *init_vector_64bit, void *data, const uint32
 			(((uint8_t*)data) + num_of_encrypted_bytes)[i] ^= vector[i];
 		}
 
-		_TEA_64bit_Block_Encrypt(data + num_of_encrypted_bytes, key_128bit);
+		_TEA_64bit_Block_Encrypt((uint8_t*)data + num_of_encrypted_bytes, key_128bit);
 
 		for (uint8_t i = 0; i < 8; ++i)
 		{
@@ -100,7 +100,7 @@ uint32_t TEA_Encrypt_CBC(const void *init_vector_64bit, void *data, const uint32
 	return num_of_encrypted_bytes;
 }
 
-uint32_t TEA_Decrypt_CBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Decrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector_1[8];
 	uint8_t vector_2[8];
@@ -119,7 +119,7 @@ uint32_t TEA_Decrypt_CBC(const void *init_vector_64bit, void *data, const uint32
 			vector_2[i] = (((uint8_t*)data) + num_of_decrypted_bytes)[i];
 		}
 
-		_TEA_64bit_Block_Decrypt(data + num_of_decrypted_bytes, key_128bit);
+		_TEA_64bit_Block_Decrypt((uint8_t*)data + num_of_decrypted_bytes, key_128bit);
 
 		for (uint8_t i = 0; i < 8; ++i)
 		{
@@ -134,7 +134,7 @@ uint32_t TEA_Decrypt_CBC(const void *init_vector_64bit, void *data, const uint32
 	return num_of_decrypted_bytes;
 }
 
-uint32_t TEA_Encrypt_PCBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Encrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector_1[8];
 	uint8_t vector_2[8];
@@ -154,7 +154,7 @@ uint32_t TEA_Encrypt_PCBC(const void *init_vector_64bit, void *data, const uint3
 			(((uint8_t*)data) + num_of_encrypted_bytes)[i] ^= vector_1[i];
 		}
 
-		_TEA_64bit_Block_Encrypt(data + num_of_encrypted_bytes, key_128bit);
+		_TEA_64bit_Block_Encrypt((uint8_t*)data + num_of_encrypted_bytes, key_128bit);
 
 		for (uint8_t i = 0; i < 8; ++i)
 		{
@@ -165,7 +165,7 @@ uint32_t TEA_Encrypt_PCBC(const void *init_vector_64bit, void *data, const uint3
 	return num_of_encrypted_bytes;
 }
 
-uint32_t TEA_Decrypt_PCBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Decrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector_1[8];
 	uint8_t vector_2[8];
@@ -184,7 +184,7 @@ uint32_t TEA_Decrypt_PCBC(const void *init_vector_64bit, void *data, const uint3
 			vector_2[i] = (((uint8_t*)data) + num_of_decrypted_bytes)[i];
 		}
 
-		_TEA_64bit_Block_Decrypt(data + num_of_decrypted_bytes, key_128bit);
+		_TEA_64bit_Block_Decrypt((uint8_t*)data + num_of_decrypted_bytes, key_128bit);
 
 		for (uint8_t i = 0; i < 8; ++i)
 		{
@@ -196,7 +196,7 @@ uint32_t TEA_Decrypt_PCBC(const void *init_vector_64bit, void *data, const uint3
 	return num_of_decrypted_bytes;
 }
 
-uint32_t TEA_Encrypt_CFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Encrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector[8];
 
@@ -221,7 +221,7 @@ uint32_t TEA_Encrypt_CFB(const void *init_vector_64bit, void *data, const uint32
 	return num_of_encrypted_bytes;
 }
 
-uint32_t TEA_Decrypt_CFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Decrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector_1[8];
 	uint8_t vector_2[8];
@@ -255,7 +255,7 @@ uint32_t TEA_Decrypt_CFB(const void *init_vector_64bit, void *data, const uint32
 	return num_of_decrypted_bytes;
 }
 
-uint32_t TEA_Encrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Encrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	uint8_t vector[8];
 
@@ -279,9 +279,10 @@ uint32_t TEA_Encrypt_OFB(const void *init_vector_64bit, void *data, const uint32
 	return num_of_encrypted_bytes;
 }
 
-uint32_t TEA_Decrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit)
+uint32_t TEA_Decrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_128bit)
 {
 	return TEA_Encrypt_OFB(init_vector_64bit, data, data_size, key_128bit);
 }
+
 
 
