6f844528543adff800a2f8625cff5b738503c6c7
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-01 18:52:33 +0300

    Add files via upload

---
+++ Libraries/Cryptography/TEA Series/RTEA/RTEA 256 bit key/rtea256bitkey.c
@@ -0,0 +1,286 @@
+
+#include "rtea256bitkey.h"
+
+
+static void _RTEA256BitKey_64bit_Block_Encrypt(void* _64bit_block, const void* key_256bit)
+{
+	uint32_t* a = &(((uint32_t*)_64bit_block)[0]);
+	uint32_t* b = &(((uint32_t*)_64bit_block)[1]);
+
+	for (int8_t i = 0; i < 64; ++i)
+	{
+		*a += *b + ((*b << 6) ^ (*b >> 8)) + ((uint32_t*)key_256bit)[i & 0b111] + i;
+
+		++i;
+
+		*b += *a + ((*a << 6) ^ (*a >> 8)) + ((uint32_t*)key_256bit)[i & 0b111] + i;
+	}
+}
+
+
+static void _RTEA256BitKey_64bit_Block_Decrypt(void* _64bit_block, const void* key_256bit)
+{
+	uint32_t* a = &(((uint32_t*)_64bit_block)[0]);
+	uint32_t* b = &(((uint32_t*)_64bit_block)[1]);
+
+	for (int8_t i = 63; i >= 0; --i)
+	{
+		*b -= *a + ((*a << 6) ^ (*a >> 8)) + ((uint32_t*)key_256bit)[i & 0b111] + i;
+
+		--i;
+
+		*a -= *b + ((*b << 6) ^ (*b >> 8)) + ((uint32_t*)key_256bit)[i & 0b111] + i;
+	}
+}
+
+
+
+uint32_t RTEA256BitKey_Encrypt_ECB(void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint32_t num_of_encrypted_bytes = 0;
+
+	for (; num_of_encrypted_bytes < data_size; num_of_encrypted_bytes += 8)
+	{
+		_RTEA256BitKey_64bit_Block_Encrypt((uint8_t*)data + num_of_encrypted_bytes, key_256bit);
+	}
+
+	return num_of_encrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Decrypt_ECB(void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint32_t num_of_decrypted_bytes = 0;
+
+	for (; num_of_decrypted_bytes < data_size; num_of_decrypted_bytes += 8)
+	{
+		_RTEA256BitKey_64bit_Block_Decrypt((uint8_t*)data + num_of_decrypted_bytes, key_256bit);
+	}
+
+	return num_of_decrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Encrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector[8];
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	uint32_t num_of_encrypted_bytes = 0;
+
+	for (; num_of_encrypted_bytes < data_size; num_of_encrypted_bytes += 8)
+	{
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			(((uint8_t*)data) + num_of_encrypted_bytes)[i] ^= vector[i];
+		}
+
+		_RTEA256BitKey_64bit_Block_Encrypt((uint8_t*)data + num_of_encrypted_bytes, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			vector[i] = (((uint8_t*)data) + num_of_encrypted_bytes)[i];
+		}
+	}
+
+	return num_of_encrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Decrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector_1[8];
+	uint8_t vector_2[8];
+
+	uint32_t num_of_decrypted_bytes = 0;
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector_1[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	for (; num_of_decrypted_bytes < data_size; num_of_decrypted_bytes += 8)
+	{
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			vector_2[i] = (((uint8_t*)data) + num_of_decrypted_bytes)[i];
+		}
+
+		_RTEA256BitKey_64bit_Block_Decrypt((uint8_t*)data + num_of_decrypted_bytes, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			(((uint8_t*)data) + num_of_decrypted_bytes)[i] ^= vector_1[i];
+
+			vector_1[i] ^= vector_2[i];
+			vector_2[i] ^= vector_1[i];
+			vector_1[i] ^= vector_2[i];
+		}
+	}
+
+	return num_of_decrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Encrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector_1[8];
+	uint8_t vector_2[8];
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector_1[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	uint32_t num_of_encrypted_bytes = 0;
+
+	for (; num_of_encrypted_bytes < data_size; num_of_encrypted_bytes += 8)
+	{
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			vector_2[i] = (((uint8_t*)data) + num_of_encrypted_bytes)[i];
+			(((uint8_t*)data) + num_of_encrypted_bytes)[i] ^= vector_1[i];
+		}
+
+		_RTEA256BitKey_64bit_Block_Encrypt((uint8_t*)data + num_of_encrypted_bytes, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			vector_1[i] = (((uint8_t*)data) + num_of_encrypted_bytes)[i] ^ vector_2[i];
+		}
+	}
+
+	return num_of_encrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Decrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector_1[8];
+	uint8_t vector_2[8];
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector_1[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	uint32_t num_of_decrypted_bytes = 0;
+
+	for (; num_of_decrypted_bytes < data_size; num_of_decrypted_bytes += 8)
+	{
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			vector_2[i] = (((uint8_t*)data) + num_of_decrypted_bytes)[i];
+		}
+
+		_RTEA256BitKey_64bit_Block_Decrypt((uint8_t*)data + num_of_decrypted_bytes, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			(((uint8_t*)data) + num_of_decrypted_bytes)[i] ^= vector_1[i];
+			vector_1[i] = (((uint8_t*)data) + num_of_decrypted_bytes)[i] ^ vector_2[i];
+		}
+	}
+
+	return num_of_decrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Encrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector[8];
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	uint32_t num_of_encrypted_bytes = 0;
+
+	for (; num_of_encrypted_bytes < data_size; num_of_encrypted_bytes += 8)
+	{
+		_RTEA256BitKey_64bit_Block_Encrypt(vector, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			(((uint8_t*)data) + num_of_encrypted_bytes)[i] ^= vector[i];
+			vector[i] = (((uint8_t*)data) + num_of_encrypted_bytes)[i];
+		}
+	}
+
+	return num_of_encrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Decrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector_1[8];
+	uint8_t vector_2[8];
+
+	uint32_t num_of_decrypted_bytes = 0;
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector_1[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	for (; num_of_decrypted_bytes < data_size; num_of_decrypted_bytes += 8)
+	{
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			vector_2[i] = (((uint8_t*)data) + num_of_decrypted_bytes)[i];
+		}
+
+		_RTEA256BitKey_64bit_Block_Encrypt(vector_1, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			(((uint8_t*)data) + num_of_decrypted_bytes)[i] ^= vector_1[i];
+
+			vector_1[i] ^= vector_2[i];
+			vector_2[i] ^= vector_1[i];
+			vector_1[i] ^= vector_2[i];
+		}
+	}
+
+	return num_of_decrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Encrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	uint8_t vector[8];
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		vector[i] = ((uint8_t*)init_vector_64bit)[i];
+	}
+
+	uint32_t num_of_encrypted_bytes = 0;
+
+	for (; num_of_encrypted_bytes < data_size; num_of_encrypted_bytes += 8)
+	{
+		_RTEA256BitKey_64bit_Block_Encrypt(vector, key_256bit);
+
+		for (uint8_t i = 0; i < 8; ++i)
+		{
+			(((uint8_t*)data) + num_of_encrypted_bytes)[i] ^= vector[i];
+		}
+	}
+
+	return num_of_encrypted_bytes;
+}
+
+
+uint32_t RTEA256BitKey_Decrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit)
+{
+	return RTEA256BitKey_Encrypt_OFB(init_vector_64bit, data, data_size, key_256bit);
+}
+
+
+
+
---
+++ Libraries/Cryptography/TEA Series/RTEA/RTEA 256 bit key/rtea256bitkey.h
@@ -0,0 +1,48 @@
+
+
+// ===============================================================================
+//
+// Ёта библиотека, реализует криптографический алгоритм RTEA
+// с размером ключа 128 бит. ‘ункции библиотеки позвол€ют использовать
+// различные режимы шифровани€ данных: ECB, CBC, PCBC, CFB и OFB
+//
+// -------------------------------------------------------------------------------
+//
+// This library implements the RTEA cryptographic algorithm
+// with a key size of 128 bits. The library functions allow you to use
+// various data encryption modes: ECB, CBC, PCBC, CFB and OFB
+// ===============================================================================
+
+
+#ifndef RTEA256BITKEY_H_
+#define RTEA256BITKEY_H_
+
+#include <stdint.h>
+
+
+uint32_t RTEA256BitKey_Encrypt_ECB(void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Decrypt_ECB(void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Encrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Decrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Encrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Decrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Encrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Decrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Encrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+uint32_t RTEA256BitKey_Decrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+
+
+#endif
+
+
+
+
