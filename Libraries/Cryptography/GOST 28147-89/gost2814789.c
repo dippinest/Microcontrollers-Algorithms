94221c3f7031a5b8144ca55151e516e8368980cf
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-03 04:46:08 +0300

    Update gost2814789.c

--- Libraries/Cryptography/GOST 28147-89/gost2814789.c
+++ Libraries/Cryptography/GOST 28147-89/gost2814789.c
@@ -9,7 +9,7 @@
 // S-block is a 4-bit replacement block. Since it is not standardized,
 // you can change its values to your own. I took these values from an
 // example in a Wikipedia article about this algorithm.
-static uint8_t STable[8][16] =
+static const uint8_t STable[8][16] =
 {
 	{0xF, 0xC, 0x2, 0xA, 0x6, 0x4, 0x5, 0x0, 0x7, 0x9, 0xE, 0xD, 0x1, 0xB, 0x8, 0x3},
 	{0xB, 0x6, 0x3, 0x4, 0xC, 0xF, 0xE, 0x2, 0x7, 0xD, 0x8, 0x0, 0x5, 0xA, 0x9, 0x1},
@@ -368,6 +368,7 @@ uint32_t GOST2814789_Decrypt_OFB(const void *init_vector_64bit, void *data, cons
 {
 	return GOST2814789_Encrypt_OFB(init_vector_64bit, data, data_size, key_256bit);
 }
+
 
 
 
