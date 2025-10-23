f53604384516e6355df842f66f5b11fe504f88b0
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-23 14:21:36 +0300

    Update main.c

--- Libraries/Cryptography/Vigenere/Examples/For AVR/Standart Vigenere/main.c
+++ Libraries/Cryptography/Vigenere/Examples/For AVR/Standart Vigenere/main.c
@@ -5,7 +5,7 @@
 #include "vigenere.h"
 
 
-// алфавит, использующийся для шифра Вижинера.
+// алфавит, использующийся для шифра Виженера.
 // Символы теста входных данных, если они отсутствуют в алфавите,
 // шифроваться не будут (аналогично с символами ключа).
 //
@@ -15,14 +15,15 @@
 // тем сложнее получится шифр.
 //
 // -------------------------------------------------------------------------------
-// this alphabet used for the Vizhiner cipher.
+// this alphabet used for the Vigеnere cipher.
 // The characters of the input data test, if they are missing from
 // the alphabet, will not be encrypted (similar to the characters of the key).
 //
 // In the alphabet, in addition to alphabetic characters, you can identify
 // the symbols of numbers, punctuation marks, and other printed characters.
 // In addition, the order of the characters can be arbitrary. The more diverse
-// and confusing the alphabet, the more complex the cipher will be//
+// and confusing the alphabet, the more complex the cipher will be
+//
 char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
 
 
@@ -78,6 +79,7 @@ int main(void)
 	{
 	}
 }
+
 
 
 
