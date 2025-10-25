bba89a3e9eb1d02d54d7ed9c73365d9c26fb960f
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-25 15:01:20 +0300

    Update xtea.h

--- Libraries/Cryptography/TEA Series/XTEA/xtea.h
+++ Libraries/Cryptography/TEA Series/XTEA/xtea.h
@@ -26,21 +26,21 @@ uint32_t XTEA_Encrypt_ECB(void *data, const uint32_t data_size, const void *key_
 
 uint32_t XTEA_Decrypt_ECB(void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Encrypt_CBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Encrypt_CBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Decrypt_CBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Decrypt_CBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Encrypt_PCBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Encrypt_PCBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Decrypt_PCBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Decrypt_PCBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Encrypt_CFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Encrypt_CFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Decrypt_CFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Decrypt_CFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Encrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Encrypt_OFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
-uint32_t XTEA_Decrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
+uint32_t XTEA_Decrypt_OFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
 #endif
 
