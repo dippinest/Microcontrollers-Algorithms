2d4fe6ec45adae1fb0c24b78f45ed8387b1cd0fb
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-25 15:43:10 +0300

    Update rtea256bitkey.h

--- Libraries/Cryptography/TEA Series/RTEA/RTEA 256 bit key/rtea256bitkey.h
+++ Libraries/Cryptography/TEA Series/RTEA/RTEA 256 bit key/rtea256bitkey.h
@@ -24,25 +24,24 @@ uint32_t RTEA256BitKey_Encrypt_ECB(void* data, const uint32_t data_size, const v
 
 uint32_t RTEA256BitKey_Decrypt_ECB(void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Encrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Encrypt_CBC(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Decrypt_CBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Decrypt_CBC(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Encrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Encrypt_PCBC(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Decrypt_PCBC(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Decrypt_PCBC(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Encrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Encrypt_CFB(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Decrypt_CFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Decrypt_CFB(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Encrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Encrypt_OFB(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
-uint32_t RTEA256BitKey_Decrypt_OFB(const void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
+uint32_t RTEA256BitKey_Decrypt_OFB(void* init_vector_64bit, void* data, const uint32_t data_size, const void* key_256bit);
 
 
 #endif
-
 
 
 
