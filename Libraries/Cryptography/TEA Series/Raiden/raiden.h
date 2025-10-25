d945c796592210d46ef71a063426a801cf8dbf1d
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-25 15:51:31 +0300

    Update raiden.h

--- Libraries/Cryptography/TEA Series/Raiden/raiden.h
+++ Libraries/Cryptography/TEA Series/Raiden/raiden.h
@@ -24,21 +24,21 @@ uint32_t RAIDEN_Encrypt_ECB(void *data, const uint32_t data_size, const void *ke
 
 uint32_t RAIDEN_Decrypt_ECB(void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Encrypt_CBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Encrypt_CBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Decrypt_CBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Decrypt_CBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Encrypt_PCBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Encrypt_PCBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Decrypt_PCBC(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Decrypt_PCBC(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Encrypt_CFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Encrypt_CFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Decrypt_CFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Decrypt_CFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Encrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Encrypt_OFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-uint32_t RAIDEN_Decrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
+uint32_t RAIDEN_Decrypt_OFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
 #endif
 
