8c78ac4da52822ab8ea409e238663a139a3c714c
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-26 17:13:45 +0300

    Update rtea256bitkey.c

--- Libraries/Cryptography/TEA Series/RTEA/RTEA 256 bit key/rtea256bitkey.c
+++ Libraries/Cryptography/TEA Series/RTEA/RTEA 256 bit key/rtea256bitkey.c
@@ -1,5 +1,7 @@
 
+
 #include "rtea256bitkey.h"
+
 
 
 static void _RTEA256BitKey_64bit_Block_Encrypt(void* _64bit_block, const void* key_256bit)
@@ -258,6 +260,69 @@ uint32_t RTEA256BitKey_Decrypt_OFB(void* init_vector_64bit, void* data, const ui
 {
 	return RTEA256BitKey_Encrypt_OFB(init_vector_64bit, data, data_size, key_256bit);
 }
+
+
+// ===============================================================================
+
+
+void *RTEA256BitKey_Encrypt_CTR(void *init_vector_64bit, void *_64bit_block, const void *key_256bit)
+{
+	uint32_t *_32bit_vector_left_path  = &(((uint32_t*)init_vector_64bit)[0]);
+	uint32_t *_32bit_vector_right_path = &(((uint32_t*)init_vector_64bit)[1]);
+	
+	
+	_RTEA256BitKey_64bit_Block_Encrypt(_64bit_block, key_256bit);
+	
+	
+	//
+	// В данной реализации счётчик на основе
+	// инициализирующего вектора инкрементируется полностью
+	//
+	// ===============================================================================
+	//
+	// In this implementation, the counter is fully
+	// incremented based on the initializing vector
+	//
+	++(*_32bit_vector_right_path);
+	
+	if (*_32bit_vector_right_path == 0)
+	{
+		++(*_32bit_vector_left_path);
+	}
+
+	return _64bit_block;
+}
+
+void *RTEA256BitKey_Decrypt_CTR(void *init_vector_64bit, void *_64bit_block, const void *key_256bit)
+{
+	uint32_t *_32bit_vector_left_path  = &(((uint32_t*)init_vector_64bit)[0]);
+	uint32_t *_32bit_vector_right_path = &(((uint32_t*)init_vector_64bit)[1]);
+	
+	
+	_RTEA256BitKey_64bit_Block_Decrypt(_64bit_block, key_256bit);
+	
+	
+	//
+	// В данной реализации счётчик на основе
+	// инициализирующего вектора инкрементируется полностью
+	//
+	// ===============================================================================
+	//
+	// In this implementation, the counter is fully
+	// incremented based on the initializing vector
+	//
+	++(*_32bit_vector_right_path);
+	
+	if (*_32bit_vector_right_path == 0)
+	{
+		++(*_32bit_vector_left_path);
+	}
+
+	return _64bit_block;
+}
+
+
+
 
 
 
