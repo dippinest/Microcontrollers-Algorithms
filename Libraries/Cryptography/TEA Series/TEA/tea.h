b9142132366e93eef6b1df9565b7e742738fd516
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-26 16:28:36 +0300

    Update tea.h

--- Libraries/Cryptography/TEA Series/TEA/tea.h
+++ Libraries/Cryptography/TEA Series/TEA/tea.h
@@ -4,13 +4,13 @@
 //
 // Эта библиотека, реализует криптографический алгоритм TEA.
 // Функции библиотеки позволяют использовать различные режимы
-// шифрования данных: ECB, CBC, PCBC, CFB и OFB
+// шифрования данных: ECB, CBC, PCBC, CFB, OFB и CTR
 //
 // -------------------------------------------------------------------------------
 //
 // This library implements the TEA cryptographic algorithm.
 // The library functions allow you to use various data
-// encryption modes: ECB, CBC, PCBC, CFB and OFB
+// encryption modes: ECB, CBC, PCBC, CFB, OFB and CTR
 //
 // ===============================================================================
 
@@ -52,6 +52,7 @@ void *TEA_Decrypt_CTR(void *init_vector_64bit, void *_64bit_block, const void *k
 
 
 #endif
+
 
 
 
