7f14290493a531db8fab33c3b23c5534aeb92a12
Denis Chicherov <dippinest.workmail@gmail.com>
2025-10-26 17:04:30 +0300

    Update raiden.h

--- Libraries/Cryptography/TEA Series/Raiden/raiden.h
+++ Libraries/Cryptography/TEA Series/Raiden/raiden.h
@@ -4,13 +4,13 @@
 //
 // Эта библиотека, реализует криптографический алгоритм Raiden.
 // Функции библиотеки позволяют использовать различные режимы
-// шифрования данных: ECB, CBC, PCBC, CFB и OFB
+// шифрования данных: ECB, CBC, PCBC, CFB, OFB и CTR
 //
 // -------------------------------------------------------------------------------
 //
 // This library implements the Raiden cryptographic algorithm.
 // The library functions allow you to use various data
-// encryption modes: ECB, CBC, PCBC, CFB and OFB
+// encryption modes: ECB, CBC, PCBC, CFB, OFB and CTR
 //
 // ===============================================================================
 
@@ -40,6 +40,16 @@ uint32_t RAIDEN_Encrypt_OFB(void *init_vector_64bit, void *data, const uint32_t 
 
 uint32_t RAIDEN_Decrypt_OFB(void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
+
+// ===============================================================================
+
+
+void *RAIDEN_Encrypt_CTR(void *init_vector_64bit, void *_64bit_block, const void *key_128bit);
+
+void *RAIDEN_Decrypt_CTR(void *init_vector_64bit, void *_64bit_block, const void *key_128bit);
+
+
 #endif
+
 
 
