8077f2ada8f637f65f82efecce0d50084c890e88
Denis Chicherov <dippinest.workmail@gmail.com>
2025-11-06 09:48:28 +0300

    Update main.c

--- Libraries/Data Conversion/base coding standards/base8/Examples/For AVR/main.c
+++ Libraries/Data Conversion/base coding standards/base8/Examples/For AVR/main.c
@@ -21,7 +21,7 @@
 // -------------------------------------------------------------------------------
 // defining arrays for encoded and decoded data
 //
-char encoded_data_buf[DATA_SIZE * 2]; // // overhead = 166%
+char encoded_data_buf[DATA_SIZE * 2]; // overhead = 166%
 char decoded_data_buf[DATA_SIZE];
 
 
@@ -66,6 +66,7 @@ int main(void)
 	{
 	}
 }
+
 
 
 
