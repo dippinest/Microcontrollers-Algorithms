2c9525c200614413fa130c047856d414002316cc
Denis Chicherov <dippinest.workmail@gmail.com>
2025-12-05 17:00:38 +0300

    Update main.c

--- Libraries/Feedback control/Comparator/Examples/For AVR/Simple temperature controller/main.c
+++ Libraries/Feedback control/Comparator/Examples/For AVR/Simple temperature controller/main.c
@@ -46,6 +46,23 @@ inline void Heater_Off()
 }
 
 
+// функции аварийного отключения нагрузки
+// (вызывается при неисправности термодатчика и прочих аварийных ситуациях)
+//
+// -------------------------------------------------------------------------------
+//
+// emergency load shutdown functions
+// (triggered in case of malfunction of the temperature sensor
+// and other emergency situations)
+//
+inline void Heater_Emergency_Shutdown()
+{
+	PORTB &= ~(1 << 1);
+}
+
+
+
+
 // переменная состояния компаратора
 //
 // -------------------------------------------------------------------------------
@@ -53,6 +70,60 @@ inline void Heater_Off()
 // comparator state variable
 //
 bool comparator_state = false;
+
+
+// определение значений переменных:
+//
+// target_temp - целевое значение температуры (его можно менять извне)
+// current_temp - текущее значение температуры (получается от датчика)
+//
+// -------------------------------------------------------------------------------
+//
+// determining the values of variables:
+//
+// target_temp - the target temperature value (it can be changed from the outside)
+// current_temp - the current temperature value (obtained from the sensor)
+//
+int16_t target_temp = 70;
+int16_t current_temp;
+
+
+// счётчик периодов обновления значения компаратора
+// -------------------------------------------------------------------------------
+// counter for comparator value update periods
+//
+uint16_t sensor_time_counter = 0;
+
+
+// переменная, хранящая информацию о работоспособности термопары
+// -------------------------------------------------------------------------------
+// a variable that stores information about the health of the thermocouple
+//
+bool is_temperature_sensor_working_properly;
+
+
+// период опроса датчика и обновления значения компаратора (в миллисекундах)
+//
+// -------------------------------------------------------------------------------
+//
+// callback for processing a single button press (in milliseconds)
+//
+#define DELTA_T 25
+
+
+// счётчик периодов времени для получения значения от датчика температуры
+// (используемый датчик температуры на основе термопары K-типа
+// нельзя опрашивать чаще времени DELTA_T_SENSOR)
+//
+// -------------------------------------------------------------------------------
+//
+// time period counter for receiving the value from the temperature sensor
+// (the temperature sensor used based on the K-type
+// thermocouple cannot be interrogated more often than the DELTA_T_SENSOR time)
+//
+#define DELTA_T_SENSOR 250
+
+
 
 
 int main(void)
@@ -78,52 +149,6 @@ int main(void)
 	UART_Initialize(9600, true, false);
 	
 	
-	
-	// определение значений переменных:
-	//
-	// target_temp - целевое значение температуры (его можно менять извне)
-	// current_temp - текущее значение температуры (получается от датчика)
-	//
-	// -------------------------------------------------------------------------------
-	//
-	// determining the values of variables:
-	//
-	// target_temp - the target temperature value (it can be changed from the outside)
-	// current_temp - the current temperature value (obtained from the sensor)
-	//
-	int16_t target_temp = 70;
-	int16_t current_temp;
-	
-	
-	// период опроса датчика и обновления значения компаратора (в миллисекундах)
-	// -------------------------------------------------------------------------------
-	// callback for processing a single button press (in milliseconds)
-	//
-	uint16_t sensor_time_counter = 0;
-	
-	
-	// период опроса датчика и обновления значения компаратора (в миллисекундах)
-	//
-	// -------------------------------------------------------------------------------
-	//
-	// callback for processing a single button press (in milliseconds)
-	//
-	#define DELTA_T 25
-	
-	
-	// счётчик периодов времени для получения значения от датчика температуры
-	// (используемый датчик температуры на основе термопары K-типа
-	// нельзя опрашивать чаще времени DELTA_T_SENSOR)
-	//
-	// -------------------------------------------------------------------------------
-	//
-	// time period counter for receiving the value from the temperature sensor
-	// (the temperature sensor used based on the K-type
-	// thermocouple cannot be interrogated more often than the DELTA_T_SENSOR time)
-	//
-	#define DELTA_T_SENSOR 250
-	
-	
 	while (1)
 	{
 		// опрос датчика температуры с периодом DELTA_T_SENSOR
@@ -136,63 +161,73 @@ int main(void)
 		{
 			sensor_time_counter = 0;
 			
-			current_temp = (int16_t)MAX6675_Get_Temperature_In_Celsius_Integer();
+			is_temperature_sensor_working_properly = MAX6675_Get_Temperature_In_Celsius_Integer( (uint16_t*)(&current_temp) );
 		}
 		
 		++sensor_time_counter;
 		
 		
 		
-		// отправка данных на плоттер
-		// (для визуализации процесса регулирования температуры)
+		// опрос датчика температуры с периодом DELTA_T_SENSOR
 		//
 		// -------------------------------------------------------------------------------
 		//
-		// sending data to the plotter
-		// (to visualize the temperature control process)
+		// polling a temperature sensor with a DELTA_T_SENSOR period
 		//
-		UART_Data_Transmit(&target_temp,  sizeof(int16_t));
-		UART_Data_Transmit(&current_temp, sizeof(int16_t));
-		
-		
-		
-		// если значение current_temp станет равным или превысит значение
-		// (target_temp + high_hysteresys), то компаратор перейдёт в состояние
-		// COMPARATOR_OUTPUT_IS_HIGH (true)
-		//
-		// если значение current_temp станет равным или снизится ниже значения
-		// (target_temp + low_hysteresys),
-		// то компаратор перейдёт в состояние COMPARATOR_OUTPUT_IS_LOW (false)
-		//
-		// в зависимости от состояния компаратора включаем либо отключаем нагрузку
-		//
-		//
-		// значения гистерезиса (high_hysteresys и low_hysteresys)
-		// подобраны экспериментально для используемого нагревателя
-		//
-		// -------------------------------------------------------------------------------
-		//
-		// if the current_temp value becomes equal to or exceeds the value
-		// (target_temp + high_hysteresys), the comparator will switch
-		// to the COMPARATOR_OUTPUT_IS_HIGH (true) state
-		//
-		// if the current_temp value becomes equal to or drops below the value
-		// (target_temp + low_hysteresys) (500 + (-50)),
-		// the comparator will switch to the COMPARATOR_OUTPUT_IS_LOW (false) state
-		//
-		// depending on the state of the comparator, we turn on or turn off the load
-		//
-		//
-		// the hysteresis values (high_hysteresys and low_hysteresys)
-		// were selected experimentally for the heater used
-		//
-		if (Comparator_Int16_Update_And_Get_Output_Value(&comparator_state, current_temp, target_temp, -1, -2) == COMPARATOR_OUTPUT_IS_HIGH)
+		if (is_temperature_sensor_working_properly)
 		{
-			Heater_Off();
-		}
-		else
-		{
-			Heater_On();
+		
+			// отправка данных на плоттер
+			// (для визуализации процесса регулирования температуры)
+			//
+			// -------------------------------------------------------------------------------
+			//
+			// sending data to the plotter
+			// (to visualize the temperature control process)
+			//
+			UART_Data_Transmit(&target_temp,  sizeof(int16_t));
+			UART_Data_Transmit(&current_temp, sizeof(int16_t));
+			
+			
+			
+			// если значение current_temp станет равным или превысит значение
+			// (target_temp + high_hysteresys), то компаратор перейдёт в состояние
+			// COMPARATOR_OUTPUT_IS_HIGH (true)
+			//
+			// если значение current_temp станет равным или снизится ниже значения
+			// (target_temp + low_hysteresys),
+			// то компаратор перейдёт в состояние COMPARATOR_OUTPUT_IS_LOW (false)
+			//
+			// в зависимости от состояния компаратора включаем либо отключаем нагрузку
+			//
+			//
+			// значения гистерезиса (high_hysteresys и low_hysteresys)
+			// подобраны экспериментально для используемого нагревателя
+			//
+			// -------------------------------------------------------------------------------
+			//
+			// if the current_temp value becomes equal to or exceeds the value
+			// (target_temp + high_hysteresys), the comparator will switch
+			// to the COMPARATOR_OUTPUT_IS_HIGH (true) state
+			//
+			// if the current_temp value becomes equal to or drops below the value
+			// (target_temp + low_hysteresys) (500 + (-50)),
+			// the comparator will switch to the COMPARATOR_OUTPUT_IS_LOW (false) state
+			//
+			// depending on the state of the comparator, we turn on or turn off the load
+			//
+			//
+			// the hysteresis values (high_hysteresys and low_hysteresys)
+			// were selected experimentally for the heater used
+			//
+			if (Comparator_Int16_Update_And_Get_Output_Value(&comparator_state, current_temp, target_temp, -1, -2) == COMPARATOR_OUTPUT_IS_HIGH)
+			{
+				Heater_Off();
+			}
+			else
+			{
+				Heater_On();
+			}
 		}
 		
 		
@@ -205,7 +240,6 @@ int main(void)
 		_delay_ms(DELTA_T);
 	}
 }
-
 
 
 
