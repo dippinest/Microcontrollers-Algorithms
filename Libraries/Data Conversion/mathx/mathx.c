41cf2be4a998ab2b8813366d26f70a02fd71a61e
Denis Chicherov <dippinest.workmail@gmail.com>
2026-07-08 07:46:52 +0300

    Update mathx.c

--- Libraries/Data Conversion/mathx/mathx.c
+++ Libraries/Data Conversion/mathx/mathx.c
@@ -3,461 +3,6 @@
 #include "mathx.h"
 
 
-int8_t MATHX_Get_Min_Int8(int8_t x1, int8_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint8_t MATHX_Get_Min_UInt8(uint8_t x1, uint8_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-int16_t MATHX_Get_Min_Int16(int16_t x1, int16_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint16_t MATHX_Get_Min_UInt16(uint16_t x1, uint16_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-int32_t MATHX_Get_Min_Int32(int32_t x1, int32_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint32_t MATHX_Get_Min_UInt32(uint32_t x1, uint32_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-
-#ifdef NUM_64BIT_SUPPORT
-
-int64_t MATHX_Get_Min_Int64(int64_t x1, int64_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint64_t MATHX_Get_Min_UInt64(uint64_t x1, uint64_t x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-#endif
-
-
-float MATHX_Get_Min_Float(float x1, float x2)
-{
-	if (x1 <= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-
-
-// ===============================================================================
-
-
-int8_t MATHX_Get_Max_Int8(int8_t x1, int8_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint8_t MATHX_Get_Max_UInt8(uint8_t x1, uint8_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-int16_t MATHX_Get_Max_Int16(int16_t x1, int16_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint16_t MATHX_Get_Max_UInt16(uint16_t x1, uint16_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-int32_t MATHX_Get_Max_Int32(int32_t x1, int32_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint32_t MATHX_Get_Max_UInt32(uint32_t x1, uint32_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-
-#ifdef NUM_64BIT_SUPPORT
-
-int64_t MATHX_Get_Max_Int64(int64_t x1, int64_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-uint64_t MATHX_Get_Max_UInt64(uint64_t x1, uint64_t x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-#endif
-
-
-float MATHX_Get_Max_Float(float x1, float x2)
-{
-	if (x1 >= x2)
-	{
-		return x1;
-	}
-	else
-	{
-		return x2;
-	}
-}
-
-
-
-// ===============================================================================
-
-
-int8_t MATHX_Get_Abs_Int8(int8_t num)
-{
-	if (num < 0)
-	{
-		return -num;
-	}
-	else
-	{
-		return num;
-	}
-}
-
-int16_t MATHX_Get_Abs_Int16(int16_t num)
-{
-	if (num < 0)
-	{
-		return -num;
-	}
-	else
-	{
-		return num;
-	}
-}
-
-int32_t MATHX_Get_Abs_Int32(int32_t num)
-{
-	if (num < 0)
-	{
-		return -num;
-	}
-	else
-	{
-		return num;
-	}
-}
-
-
-#ifdef NUM_64BIT_SUPPORT
-
-int64_t MATHX_Get_Abs_Int64(int64_t num)
-{
-	if (num < 0)
-	{
-		return -num;
-	}
-	else
-	{
-		return num;
-	}
-}
-
-#endif
-
-
-float MATHX_Get_Abs_Float(float num)
-{
-	if (num < 0)
-	{
-		return -num;
-	}
-	else
-	{
-		return num;
-	}
-}
-
-
-
-// ===============================================================================
-
-
-int8_t MATHX_Constrain_Int8(int8_t val, int8_t min, int8_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-uint8_t MATHX_Constrain_UInt8(uint8_t val, uint8_t min, uint8_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-int16_t MATHX_Constrain_Int16(int16_t val, int16_t min, int16_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-uint16_t MATHX_Constrain_UInt16(uint16_t val, uint16_t min, uint16_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-int32_t MATHX_Constrain_Int32(int32_t val, int32_t min, int32_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-uint32_t MATHX_Constrain_UInt32(uint32_t val, uint32_t min, uint32_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-
-#ifdef NUM_64BIT_SUPPORT
-
-
-int64_t MATHX_Constrain_Int64(int64_t val, int64_t min, int64_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-uint64_t MATHX_Constrain_UInt64(uint64_t val, uint64_t min, uint64_t max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-#endif
-
-
-float MATHX_Constrain_Float(float val, float min, float max)
-{
-	if (val <= min)
-	{
-		return min;
-	}
-
-	if (val >= max)
-	{
-		return max;
-	}
-
-	return val;
-}
-
-
-
-// ===============================================================================
-
 
 int32_t MATHX_Scaling_Int32(int32_t val, int32_t input_min_val, int32_t input_max_val, int32_t output_min_val, int32_t output_max_val)
 {
@@ -505,5 +50,190 @@ float MATHX_Scaling_Float(float val, float input_min_val, float input_max_val, f
 
 	return new_val;
 }
+
+
+
+// ===============================================================================
+
+
+
+uint32_t MATHX_Get_GCD_UInt32(uint32_t num1, uint32_t num2)
+{
+	uint32_t temp;
+	
+	while (num2 != 0)
+	{
+		temp = num2;
+		
+		num2 = num1 % num2;
+		
+		num1 = temp;
+	}
+	
+	return num1;
+}
+
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+uint64_t MATHX_Get_GCD_UInt64(uint64_t num1, uint64_t num2)
+{
+	uint64_t temp;
+	
+	while (num2 != 0)
+	{
+		temp = num2;
+		
+		num2 = num1 % num2;
+		
+		num1 = temp;
+	}
+	
+	return num1;
+}
+
+#endif
+
+
+
+// ===============================================================================
+
+
+
+uint32_t MATHX_Get_LCM_UInt32(uint32_t a, uint32_t b)
+{
+	uint32_t lcm = (a / MATHX_Get_GCD_UInt32(a, b)) * b;
+	
+	return lcm;
+}
+
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+uint64_t MATHX_Get_LCM_UInt64(uint64_t a, uint64_t b)
+{
+	uint64_t lcm = (a / MATHX_Get_GCD_UInt64(a, b)) * b;
+	
+	return lcm;
+}
+
+#endif
+
+
+
+// ===============================================================================
+
+
+
+bool MATHX_Is_Prime_UInt32(uint32_t num)
+{
+	if (num < 2)
+	{
+		return false;
+	}
+	
+	if ((num & 0b1) == 0)
+	{
+		return (bool)(num == 2);
+	}
+	
+	
+	for (uint32_t i = 3; (i * i) <= num; i += 2)
+	{
+		if ((num % i) == 0)
+		{
+			return false;
+		}
+	}
+	
+	
+	return true;
+}
+
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+bool MATHX_Is_Prime_UInt64(uint64_t num)
+{
+	if (num < 2)
+	{
+		return false;
+	}
+	
+	if ((num & 0b1) == 0)
+	{
+		return (bool)(num == 2);
+	}
+	
+	
+	for (uint64_t i = 3; (i * i) <= num; i += 2)
+	{
+		if ((num % i) == 0)
+		{
+			return false;
+		}
+	}
+	
+	
+	return true;
+}
+
+#endif
+
+
+
+// ===============================================================================
+
+
+
+uint32_t MATHX_Get_Factorial_UInt32(uint8_t num)
+{
+	if (num > 12)
+	{
+		return 0;
+	}
+	
+	
+	uint32_t result = 1;
+	
+	for (uint8_t i = 2; i <= num; i++)
+	{
+		result *= (uint32_t)i;
+	}
+	
+	return result;
+}
+
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+uint64_t MATHX_Get_Factorial_UInt64(uint8_t num)
+{
+	if (num > 20)
+	{
+		return 0;
+	}
+	
+	
+	uint64_t result = 1;
+	
+	for (uint8_t i = 2; i <= num; i++)
+	{
+		result *= (uint64_t)i;
+	}
+	
+	return result;
+}
+
+#endif
+
+
+
+
+
 
 
