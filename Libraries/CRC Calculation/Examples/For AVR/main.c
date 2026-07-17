3cd398411c7677097818bb185f3b2b53f68a116c
Denis Chicherov <dippinest.workmail@gmail.com>
2026-07-17 20:22:53 +0300

    Update main.c

--- Libraries/CRC Calculation/Examples/For AVR/main.c
+++ Libraries/CRC Calculation/Examples/For AVR/main.c
@@ -10,7 +10,7 @@ typedef struct
 	uint16_t u16_val_2;
 	int64_t  i64_val;
 
-} Data;
+} Data_t;
 
 static char str_buf[16];
 
@@ -22,12 +22,12 @@ int main(void)
 	ITOHEXA_Set_String_Buffer(str_buf);
 	
 	
-	Data d = { 'a', 232, 7633, 684567387573458769 };
+	Data_t d = { 'a', 232, 7633, 684567387573458769 };
 	
 	
 	UART_StringLn_Transmit(
 		ITOHEXA_32bitNum_To_HexString(
-			CRC32_Get_Value(&d, sizeof(Data), 0xA0435757, 0xFF457436, true, false, 0x00446774), false));
+			CRC32_Get_Value(&d, sizeof(Data_t), 0xA0435757, 0xFF457436, true, false, 0x00446774), false));
 	
 	while (1)
 	{
