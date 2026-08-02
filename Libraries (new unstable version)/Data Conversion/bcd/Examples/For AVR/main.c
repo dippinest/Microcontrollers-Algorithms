7d15807bc88ff3de4115811c67497036810b8b7a
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-02 17:39:53 +0300

    Add files via upload

---
+++ Libraries (new unstable version)/Data Conversion/bcd/Examples/For AVR/main.c
@@ -0,0 +1,22 @@
+
+#include <util/delay.h>
+
+#include "uart.h"
+#include "bcdcode.h"
+
+int main(void)
+{
+	UART_Initialize(9600, true, false);
+	
+	uint8_t i = 0;
+	uint8_t bcd;
+	
+	while (1)
+	{
+		bcd = BCDCODE_UInt8_To_UInt8BCD(i);
+		UART_StringFmt_Transmit("Counter: 0x%X (%d); BCD code: 0x%X; normal code: 0x%X\r\n", i, i, bcd, BCDCODE_UInt8BCD_To_UInt8(bcd));
+		++i;
+		
+		_delay_ms(200);
+	}
+}
