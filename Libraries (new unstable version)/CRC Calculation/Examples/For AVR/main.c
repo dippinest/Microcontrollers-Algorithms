feb9cbec7caca75ecfb1e73a3a73c757d477a8ce
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-05 12:12:20 +0300

    Add files via upload

---
+++ Libraries (new unstable version)/CRC Calculation/Examples/For AVR/main.c
@@ -0,0 +1,40 @@
+
+#include "uart.h"
+#include "itohexa.h"
+#include "crc.h"
+
+
+
+
+char string_buffer[16];
+
+
+typedef struct
+{
+	char c;
+	uint16_t u16_val_1;
+	uint16_t u16_val_2;
+	int64_t  i64_val;
+
+} Data_t;
+
+
+int main(void)
+{
+	UART_Initialize(9600, true, false);
+	
+	
+	Data_t d = { 'a', 232, 7633, 684567387573458769 };
+	
+	
+	UART_StringLn_Transmit(
+		ITOHEXA_32bitNum_To_HexString(string_buffer, CRC32_Get_Value(&d, sizeof(Data_t), 0xA0435757, 0xFF457436, true, false, 0x00446774), false)
+	);
+	
+	while (1)
+	{
+	}
+}
+
+
+
