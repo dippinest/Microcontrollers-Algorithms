f79bd0f10ebd022268f0f11a029c8cb1d62134b7
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-18 16:29:56 +0300

    Update ntc.c

--- Libraries (new unstable version)/Other solutions and code examples/NTC Thermometer/ntc.c
+++ Libraries (new unstable version)/Other solutions and code examples/NTC Thermometer/ntc.c
@@ -9,7 +9,7 @@ float NTC_Get_Temperature_To_Kelvin(const NTC_Data_Struct_t *ntc, float adc_valu
 	float U  = ((float)adc_value / ntc->adc_max_val) * ntc->U_ref;
 	float R  = ntc->R_serial * (U / (ntc->U_gen - U));
 	
-	float lnR = log(R);
+	float lnR = logf(R);
 	
 	return 1.0 / (ntc->A_coef + (ntc->B_coef * lnR) + (ntc->C_coef * (lnR * lnR * lnR)));
 }
