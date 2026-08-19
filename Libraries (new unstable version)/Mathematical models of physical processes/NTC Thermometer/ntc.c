c3133a12d11a7e221ba4f0d72b910e3d0d66b899
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-19 05:07:07 +0300

    Add files via upload

---
+++ Libraries (new unstable version)/Mathematical models of physical processes/NTC Thermometer/ntc.c
@@ -0,0 +1,21 @@
+
+
+#include "ntc.h"
+
+
+
+float NTC_Get_Temperature_To_Kelvin(const NTC_Data_Struct_t *ntc, float adc_value)
+{
+	float U  = ((float)adc_value / ntc->adc_max_val) * ntc->U_ref;
+	float R  = ntc->R_serial * (U / (ntc->U_gen - U));
+	
+	float lnR = logf(R);
+	
+	return 1.0 / (ntc->A_coef + (ntc->B_coef * lnR) + (ntc->C_coef * (lnR * lnR * lnR)));
+}
+
+
+
+
+
+
