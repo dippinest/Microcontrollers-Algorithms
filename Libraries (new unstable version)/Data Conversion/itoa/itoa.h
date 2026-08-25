6e2f9ef8156b55eaf81a27ebf8b6a8749992193e
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-25 04:53:25 +0300

    Update itoa.h

--- Libraries (new unstable version)/Data Conversion/itoa/itoa.h
+++ Libraries (new unstable version)/Data Conversion/itoa/itoa.h
@@ -15,7 +15,7 @@
 #define ITOA_H_
 
 #include <stdint.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
 
 
@@ -24,13 +24,13 @@
 
 
 // only for positive numbers!
-char *ITOA_UInt8_To_String_Without_Alignment(char *string_buffer, uint8_t val, int8_t num_of_chars);
+char *ITOA_UInt8_To_String_Without_Alignment(char *string_buffer, uint8_t number, int8_t num_of_chars);
 
 // only for positive numbers!
-char *ITOA_UInt16_To_String_Without_Alignment(char *string_buffer, uint16_t val, int8_t num_of_chars);
+char *ITOA_UInt16_To_String_Without_Alignment(char *string_buffer, uint16_t number, int8_t num_of_chars);
 
 // only for positive numbers!
-char *ITOA_UInt32_To_String_Without_Alignment(char *string_buffer, uint32_t val, int8_t num_of_chars);
+char *ITOA_UInt32_To_String_Without_Alignment(char *string_buffer, uint32_t number, int8_t num_of_chars);
 
 
 
@@ -39,7 +39,7 @@ char *ITOA_UInt32_To_String_Without_Alignment(char *string_buffer, uint32_t val,
 
 
 // only for positive numbers!
-char *ITOA_UInt64_To_String_Without_Alignment(char *string_buffer, uint64_t val, int8_t num_of_chars);
+char *ITOA_UInt64_To_String_Without_Alignment(char *string_buffer, uint64_t number, int8_t num_of_chars);
 
 #endif
 
@@ -50,26 +50,26 @@ char *ITOA_UInt64_To_String_Without_Alignment(char *string_buffer, uint64_t val,
 
 
 
-char *ITOA_Int8_To_String(char *string_buffer, int8_t val, int8_t num_of_chars);
+char *ITOA_Int8_To_String(char *string_buffer, int8_t number, int8_t num_of_chars);
 
-char *ITOA_UInt8_To_String(char *string_buffer, uint8_t val, int8_t num_of_chars);
+char *ITOA_UInt8_To_String(char *string_buffer, uint8_t number, int8_t num_of_chars);
 
-char *ITOA_Int16_To_String(char *string_buffer, int16_t val, int8_t num_of_chars);
+char *ITOA_Int16_To_String(char *string_buffer, int16_t number, int8_t num_of_chars);
 
-char *ITOA_UInt16_To_String(char *string_buffer, uint16_t val, int8_t num_of_chars);
+char *ITOA_UInt16_To_String(char *string_buffer, uint16_t number, int8_t num_of_chars);
 
-char *ITOA_Int32_To_String(char *string_buffer, int32_t val, int8_t num_of_chars);
+char *ITOA_Int32_To_String(char *string_buffer, int32_t number, int8_t num_of_chars);
 
-char *ITOA_UInt32_To_String(char *string_buffer, uint32_t val, int8_t num_of_chars);
+char *ITOA_UInt32_To_String(char *string_buffer, uint32_t number, int8_t num_of_chars);
 
 
 
 
 #ifdef NUM_64BIT_SUPPORT
 
-char *ITOA_Int64_To_String(char *string_buffer, int64_t val, int8_t num_of_chars);
+char *ITOA_Int64_To_String(char *string_buffer, int64_t number, int8_t num_of_chars);
 
-char *ITOA_UInt64_To_String(char *string_buffer, uint64_t val, int8_t num_of_chars);
+char *ITOA_UInt64_To_String(char *string_buffer, uint64_t number, int8_t num_of_chars);
 
 #endif
 
