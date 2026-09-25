376b0fea9c891c9a92c44f33804497146bb54905
Denis Chicherov <dippinest.workmail@gmail.com>
2026-09-25 19:49:25 +0300

    Update stringx.h

--- Libraries (new unstable version)/Data Conversion/stringx/stringx.h
+++ Libraries (new unstable version)/Data Conversion/stringx/stringx.h
@@ -212,64 +212,64 @@ static inline char STRINGX_Get_Numerical_Digit_Char_By_Pos(const uint8_t pos)
 
 
 
-bool STRINGX_Is_String_Contains_Only_Letter(const char *str);
+bool STRINGX_Is_String_Contains_Only_Letter_Chars(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_Letter_Safe(const char *str, const uint16_t str_max_size);
-
-
-bool STRINGX_Is_String_Contains_Only_Digit(const char *str);
-
-bool STRINGX_Is_String_Contains_Only_Digit_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_Letter_Chars_Safe(const char *str, const uint16_t str_max_size);
 
 
-bool STRINGX_Is_String_Contains_Only_HEXDigit(const char *str);
+bool STRINGX_Is_String_Contains_Only_Digit_Chars(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_HEXDigit_Safe(const char *str, const uint16_t str_max_size);
-
-
-bool STRINGX_Is_String_Contains_Only_Letter_Or_Digit(const char *str);
-
-bool STRINGX_Is_String_Contains_Only_Letter_Or_Digit_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_Digit_Chars_Safe(const char *str, const uint16_t str_max_size);
 
 
-bool STRINGX_Is_String_Contains_Only_Whitespace_Or_HTab(const char *str);
+bool STRINGX_Is_String_Contains_Only_HEXDigit_Chars(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_Whitespace_Or_HTab_Safe(const char *str, const uint16_t str_max_size);
-
-
-bool STRINGX_Is_String_Contains_Only_Control_Char_CNTRL(const char *str);
-
-bool STRINGX_Is_String_Contains_Only_Control_Char_CNTRL_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_HEXDigit_Chars_Safe(const char *str, const uint16_t str_max_size);
 
 
-bool STRINGX_Is_String_Contains_Only_Printable(const char *str);
+bool STRINGX_Is_String_Contains_Only_Letters_Chars_Or_Digit_Chars(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_Printable_Safe(const char *str, const uint16_t str_max_size);
-
-
-bool STRINGX_Is_String_Contains_Only_Printable_And_Not_Whitespace(const char *str);
-
-bool STRINGX_Is_String_Contains_Only_Printable_And_Not_Whitespace_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_Letters_Chars_Or_Digit_Chars_Safe(const char *str, const uint16_t str_max_size);
 
 
-bool STRINGX_Is_String_Contains_Only_Space(const char *str);
+bool STRINGX_Is_String_Contains_Only_Whitespace_Chars_Or_HTab_Chars(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_Space_Safe(const char *str, const uint16_t str_max_size);
-
-
-bool STRINGX_Is_String_Contains_Only_Punctuation_Mark(const char *str);
-
-bool STRINGX_Is_String_Contains_Only_Punctuation_Mark_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_Whitespace_Chars_Or_HTab_Chars_Safe(const char *str, const uint16_t str_max_size);
 
 
-bool STRINGX_Is_String_Contains_Only_Upper_Case(const char *str);
+bool STRINGX_Is_String_Contains_Only_Control_Chars_CNTRL(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_Upper_Case_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_Control_Chars_CNTRL_Safe(const char *str, const uint16_t str_max_size);
 
 
-bool STRINGX_Is_String_Contains_Only_Lower_Case(const char *str);
+bool STRINGX_Is_String_Contains_Only_Printable_Chars(const char *str);
 
-bool STRINGX_Is_String_Contains_Only_Lower_Case_Safe(const char *str, const uint16_t str_max_size);
+bool STRINGX_Is_String_Contains_Only_Printable_Chars_Safe(const char *str, const uint16_t str_max_size);
+
+
+bool STRINGX_Is_String_Contains_Only_Printable_Chars_And_Not_Whitespaces(const char *str);
+
+bool STRINGX_Is_String_Contains_Only_Printable_Chars_And_Not_Whitespaces_Safe(const char *str, const uint16_t str_max_size);
+
+
+bool STRINGX_Is_String_Contains_Only_Space_Chars(const char *str);
+
+bool STRINGX_Is_String_Contains_Only_Space_Chars_Safe(const char *str, const uint16_t str_max_size);
+
+
+bool STRINGX_Is_String_Contains_Only_Punctuation_Mark_Chars(const char *str);
+
+bool STRINGX_Is_String_Contains_Only_Punctuation_Mark_Chars_Safe(const char *str, const uint16_t str_max_size);
+
+
+bool STRINGX_Is_String_Contains_Only_Upper_Case_Chars(const char *str);
+
+bool STRINGX_Is_String_Contains_Only_Upper_Case_Chars_Safe(const char *str, const uint16_t str_max_size);
+
+
+bool STRINGX_Is_String_Contains_Only_Lower_Case_Chars(const char *str);
+
+bool STRINGX_Is_String_Contains_Only_Lower_Case_Chars_Safe(const char *str, const uint16_t str_max_size);
 
 
 
